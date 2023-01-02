20c8ea747a4d1d0b9431415cc9c6d5ffecc9ef33
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2023-01-03 00:56:32 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+// #include <iostream.h>
+using namespace std;
+
+#define debug(x) cout << #x << " " << x <<'\n';
+#define fo(i, n) for(int i = 0; i < n; i++)
+
+
+
+void solve(){
+  int i, j, k, l, m, n, h, k;
+
+}
+    
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
