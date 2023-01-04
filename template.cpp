03099af55ef25715d9e769a5ede18cc56238c621
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2023-01-04 23:29:51 +0530

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
-// #include <iostream.h>
 using namespace std;
 
 #define debug(x) cout << #x << " " << x <<'\n';
-#define fo(i, n) for(int i = 0; i < n; i++)
-
+#define fo(i, k, n) for(int i = k; i < n; i++);
 
 
 void solve(){
-  int i, j, k, l, m, n, h, k;
+  int a, b, c, d, i, j, k, l, m, n, h;
 
 }
     
