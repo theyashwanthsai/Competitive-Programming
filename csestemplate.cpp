ca33305fa999f2360a9f07177bdd0464fb930ad6
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2023-07-19 09:59:50 +0530

    Add files via upload

---
+++ csestemplate.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cout << #x << " " << x <<'\n';
+
+
+void solve(){
+    long long a, b, c, d, i, j, k, l, m, n, h;
+}
+    
+int main(){
+    solve();
+}
+
